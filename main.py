8137674a5c600afadfaf1714e758b86fed813677
Erbol <erboshaazhybekov@gmail.com>
2022-05-22 01:54:16 +0600

    clock

---
+++ main.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import requests
+from requests import get
+import time 
+import random
+
+
+url = 'https://www.kivano.kg/kompyutery?page='
+computers = []
+count = 1
+while count <= 5:
+    url = 'https://www.kivano.kg/kompyutery?page=' + str(count)
+    print(url)
+    response = get(url)
+    html_soup = BeautifulSoup(response.text, "html.parser")
+
+    computers_data = html_soup.find_all('div', class_='item product_listbox oh')
+    if computers_data != []:
+        computers.extend(computers_data)
+        value = random.random()
+        scaled_value = 1 + (value * (9 - 5))
+        print(scaled_value)
+        time.sleep(scaled_value)
+    else:
+        print('empty')
+        break
+    count += 1
+
+print(len(computers))
+print(computers[1])
+print()
+n = int(len(computers)) - 1
+count = 0
+while count <= 2:
+    info = computers[int(count)]
+    price = info.find('div', {'class':'listbox_price text-center'}).text
+    title = info.find('div', {'class':'listbox_title oh'}).text
+    print(title,' ', price)
+    count += 1
+        
